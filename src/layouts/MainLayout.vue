8e6e38147d5c453f408e95b6dcbd78a2974c19d2
asitikovdo <asitikovdo@gmail.com>
2022-10-13 19:03:29 +0900

    fix q-drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -21,20 +21,21 @@
 
     <q-drawer
       v-model="leftDrawerOpen"
-
+      persistent="false"
       bordered
     >
       <q-list>
         <q-item-label
           header
         >
-          Essential Links
+          Основные разделы
         </q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
           :key="link.title"
           v-bind="link"
+          @click="toggleLeftDrawer"
         />
       </q-list>
     </q-drawer>
@@ -50,47 +51,18 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
+    title: 'Карта',
+    caption: 'г. Благовещенск',
     icon: 'school',
-    link: 'https://quasar.dev'
+    link: '/#'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
+    title: 'Анализ данных',
+    // caption: 'github.com/quasarframework',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
+    link: '/#/charts'
   },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+
 ];
 
 import { defineComponent, ref } from 'vue'
